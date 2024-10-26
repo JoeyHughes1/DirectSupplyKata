2edfd34920810f91c94ce401d553fbc23cbe9520
Joey Hughes <jehughe3@ncsu.edu>
2024-10-26 01:53:28 -0400

    Initial Commit: Project Files and Initial Notes
    I have done a lot of research and wrote down all my thoughts along the way in the Notes document, but in terms of concrete things I'm committing, it's just all the Notes and the initial state of the Visual Studio project.

---
+++ DirectSupplyQuizKata/QuizCL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+/// <summary>
+/// Not too sure yet.
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    /// The Main function for the program. Runs the command line interface for interacting with quizzes.
+    /// </summary>
+    private static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(Directory.GetCurrentDirectory());
+    }
+}
